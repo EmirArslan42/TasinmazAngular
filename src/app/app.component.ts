785e520aa8e2ad17b3610dc524ed95d63d5ab585
emirarslan <emirarslan179@gmail.com>
2025-12-06 17:49:15 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tasinmaz-frontend';
+
+  name="Emir";
+  isUser=true;
+
+  constructor(){
+    console.log(`name: ${this.name}`);
+    //console.log("name: "+this.name);
+    console.log(`isUser: ${this.isUser}`);
+  }
+
+  MethodOrnek(){ //method içinde değişken tanımlama let,var,const ile yapılır
+    let city:string="Istanbul";
+    console.log(`city: ${city}`);
+  }
+  /*
+  createdDare:Date=new Date();
+  names:string[]=["Emir","Dilay","Eren"];
+  isUsers:boolean[]=[true,false,true,false];
+  //deneme:string=1; bu hata almamıza sebep olacaktır
+  type:any=1; //any türü her türlü değeri alabilir
+  type1:unknown=1; //unknown türü herhangi bir değeri alabilir ama o değeri kullanmadan önce tür kontrolü yapmalıyız
+
+  Method1(){
+    this.type.toFixed();
+    //this.type1.toFixed(); //hata verir
+    (this.type1 as number).toFixed(); //hata vermez çünkü tür kontrolü yapıldı
+  }
+*/
 }
